29f794857ab500ff130bc56b118617222f2fdf04
dipen <dipen.2052@gmail.com>
2021-07-27 23:21:51 +0545

    user model chaend

--- home/models.py
+++ home/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import json
-# Create your models here.
-User = get_user_model()
+from django.contrib.auth.models import User
+
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     notification = models.TextField(max_length=100)
